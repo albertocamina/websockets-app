e8617736c8cc9a71173187846450431cd2facf5e
albertocamina <alberto.camina@sngular.com>
2021-11-15 16:31:48 +0100

    Clave Secreta Escondida

--- app/websockets_app/settings.py
+++ app/websockets_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87shgrx=1$#yu=$dq^nr04@mmivy+1@-%^r73nxijj0bi*u-iy'
+SECRET_KEY = os.getenv( "SECRET_KEY", "" )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
